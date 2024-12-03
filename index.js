1b616054d4ec6d69b497b64c82d746df7dae60be
Igorraiad0 <igorsonic56@gmail.com>
2024-12-03 15:01:09 -0300

    Resolvendo problemas deimportação de bibliotecas ( import's)

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-import express from 'express'
-const app = express()
-import patch from 'patch'
-import{fileURLToPath} from 'url'
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const app = express();
 
-app.use(patch({patchDir: __dirname}))  
-app.use(express.static(patch.join, 'public'))     
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/conteudoHTML/index.html')
